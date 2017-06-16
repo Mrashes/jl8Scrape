00f15395fb34d07a919fc9f63e7afb860b5f39b4
Max Rashes <mrashes2@gmail.com>
2017-06-16 16:14:00 -0500

    installing anaconda so new Packages

--- init.py
+++ init.py
@@ -1,13 +1,12 @@
 from bs4 import BeautifulSoup
-from urllib2 import urlopen
 import urllib
 
 # use this image scraper from the location that 
 #you want to save scraped images to
 
 def make_soup(url):
-	html = urlopen(url).read()
-	return BeautifulSoup(html)
+	html = urllib.request.urlopen(url).read()
+	return BeautifulSoup(html, "html.parser")
 
 def get_images(url):
 	soup = make_soup(url)
@@ -17,13 +16,16 @@ def get_images(url):
 	print 'Downloading images to current working directory.'
 	#compile our unicode list of image links
 	image_links = [each.get('href') for each in links]
-	for each in image_links:
+	# remove non image files
+	new_image_links = image_links[5:]
+	for each in new_image_links:
+		# instance of teh website's name
 		filename="http://limbero.org/jl8/comics/" + each[-1]
-		urllib.urlretrieve(each, filename)
+		urllib.request.urlretrieve.urlretrieve(each, filename)
 		
-	return image_links
+	return new_image_links
 
 	# I need to find a way to skip the first couple of links as they aren't images
 
 #a standard call looks like this
-#get_images('http://www.wookmark.com')
+#
